a84de59d6ded47bdc07c516f75e8724c2bf29147
Nikki Murello <nmurello15@gmail.com>
2022-10-31 13:01:55 -0400

    forgot to delete obsolete commented code block

--- src/c/animation/anim.c
+++ src/c/animation/anim.c
@@ -15,53 +15,6 @@ static void s_set_animation_things(Animation *anim, int delay, int duration) {
     animation_set_duration(anim, duration);
 }
 
-/*
-// sets all animation variables and properties
-void set_anim_props(GRect bounds_full, int delay_ms, int duration_ms, int onscreen_ms) {
-    // makes the coordinates for the animation
-    GRect date_start_rect = GRect(0, 50, bounds_full.size.w, bounds_full.size.h);
-    GRect date_end_rect = GRect(0, 0, bounds_full.size.w, bounds_full.size.h);
-
-    GRect time_start_rect = GRect(0, 0, bounds_full.size.w, bounds_full.size.h);
-    GRect time_end_rect = GRect(0, -10, bounds_full.size.w, bounds_full.size.h);
-
-    GRect bat_start_rect = GRect(0, 7, bounds_full.size.w, bounds_full.size.h);
-    GRect bat_end_rect = GRect(0, 0, bounds_full.size.w, bounds_full.size.h);
-
-    // define property animations
-    PropertyAnimation *prop_anim_date_start = property_animation_create_layer_frame(date_layer, &date_start_rect, &date_end_rect);
-    PropertyAnimation *prop_anim_date_end = property_animation_create_layer_frame(date_layer, &date_end_rect, &date_start_rect);
-
-    PropertyAnimation *prop_anim_time_start = property_animation_create_layer_frame(time_layer, &time_start_rect, &time_end_rect);
-    PropertyAnimation *prop_anim_time_end = property_animation_create_layer_frame(time_layer, &time_end_rect, &time_start_rect);
-
-    PropertyAnimation *prop_anim_bat_start = property_animation_create_layer_frame(bat_layer, &bat_start_rect, &bat_end_rect);
-    PropertyAnimation *prop_anim_bat_end = property_animation_create_layer_frame(bat_layer, &bat_end_rect, &bat_start_rect);
-
-    // sets the property animations to each animation variable
-    date_anim_start = property_animation_get_animation(prop_anim_date_start);
-    date_anim_end = property_animation_get_animation(prop_anim_date_end);
-
-    time_anim_start = property_animation_get_animation(prop_anim_time_start);
-    time_anim_end = property_animation_get_animation(prop_anim_time_end);
-
-    bat_anim_start = property_animation_get_animation(prop_anim_bat_start);
-    bat_anim_end = property_animation_get_animation(prop_anim_bat_end);
-
-    // date set things
-    s_set_animation_things(date_anim_start, delay_ms, duration_ms);
-    s_set_animation_things(date_anim_end, onscreen_ms, duration_ms);
-
-    // time set things
-    s_set_animation_things(time_anim_start, delay_ms, duration_ms);
-    s_set_animation_things(time_anim_end, onscreen_ms, duration_ms);
-
-    // bat bar set things
-    s_set_animation_things(bat_anim_start, delay_ms, duration_ms);
-    s_set_animation_things(bat_anim_end, onscreen_ms, duration_ms);
-}
-*/
-
 Animation *anim_set_props(GRect frame_start, GRect frame_end, Layer *layer, int delay_ms, int duration_ms) {
     PropertyAnimation *prop_anim = property_animation_create_layer_frame(layer, &frame_start, &frame_end);
     Animation *return_anim = property_animation_get_animation(prop_anim);
